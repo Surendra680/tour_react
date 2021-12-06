639a576c940d51d28b7c46c75d6d83c555624041
Surendra680 <surendramaurya680@gmail.com>
2021-12-06 13:55:27 +0530

    Updates

---
+++ precache-manifest.8d64f3f0edc4e6c5a947d0802e7ac66e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ddc21f78ad82177ba7488c2203479fdd",
+    "url": "/tour_react/index.html"
+  },
+  {
+    "revision": "9ddb7dd7711841fac4fc",
+    "url": "/tour_react/static/css/main.8e49c9d7.chunk.css"
+  },
+  {
+    "revision": "303a7e45ea08ea333814",
+    "url": "/tour_react/static/js/2.b79bb511.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tour_react/static/js/2.b79bb511.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ddb7dd7711841fac4fc",
+    "url": "/tour_react/static/js/main.c7703858.chunk.js"
+  },
+  {
+    "revision": "8705d17c4e3b02a48851",
+    "url": "/tour_react/static/js/runtime-main.ec033fa0.js"
+  }
+]);
